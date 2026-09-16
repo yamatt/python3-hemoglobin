68f0f30ff63d363ba18b3333778e4ee4e9d02f36
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2026-09-16 21:12:31 +0100

    Make language choice a choice

--- hemoglobin/__main__.py
+++ hemoglobin/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from .hemoglobin import Hemoglobin
+from .grammarbot import Language
 from .rendering import render_human, render_json
 
 
@@ -11,7 +12,12 @@ from .rendering import render_human, render_json
     envvar="HEMOGLOBIN_GRAMMARBOT_API_KEY",
 )
 @click.option("--path", help="Where to find these files to parse.", multiple=True)
-@click.option("--language", help="Language to use.", default="en_US")
+@click.option(
+    "--language",
+    help="Language to use.",
+    default="en_US",
+    type=click.Choice(Language, case_sensitive=False),
+)
 @click.option("--use-json", "use_json", is_flag=True, help="Use JSON output")
 def main(apikey, path, language, use_json):
     hemoglobin = Hemoglobin(apikey=apikey, paths=path, language=language)
